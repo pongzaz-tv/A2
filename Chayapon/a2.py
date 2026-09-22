014589e7f3deb008da4c3bf569ef3f43dc12cf51
Chayapon Yaram <pongisnotgood3@gmail.com>
2026-09-22 12:37:19 +0700

    add check grid

--- Chayapon/a2.py
+++ Chayapon/a2.py
@@ -36,11 +36,48 @@ class Board:
             line(draw_cbx,draw_cby,draw_cbx+400,draw_cby)
             draw_cby += 50
             iy += 1
+    def check_grid(self,x,y):
+        matrix_x = 0
+        matrix_y = 0
+        if(x>50 and x<100):
+            matrix_x = 1
+        elif (x>100 and x<150):
+            matrix_x = 2
+        elif (x>150 and x<200):
+            matrix_x = 3
+        elif (x>200 and x<250):
+            matrix_x = 4
+        elif (x>250 and x<300):
+            matrix_x = 5
+        elif (x>300 and x<350):
+            matrix_x = 6
+        elif (x>350 and x<400):
+            matrix_x = 7
+        elif (x>400 and x<450):
+            matrix_x = 8
 
+        if(y>25 and y<75):
+            matrix_y = 1
+        elif (y>75 and y<125):
+            matrix_y = 2
+        elif (y>125 and y<175):
+            matrix_y = 3
+        elif (y>175 and y<225):
+            matrix_y = 4
+        elif (y>225 and y<275):
+            matrix_y = 5
+        elif (y>275 and y<325):
+            matrix_y = 6
+        elif (y>325 and y<375):
+            matrix_y = 7
+        elif (y>375 and y<425):
+            matrix_y = 8
+        text(matrix_y,mouseX,mouseY)
+        text(matrix_x,mouseX+4,mouseY)
 
 
 radius = 25
-"""     
+"""
 class Piece:
     def __init__():
 """
@@ -52,10 +89,11 @@ def setup():
 
 def draw():
     background(225)
-    is_mouse_pressed
     ellipse(mouseX,mouseY,radius,radius) #system tester
     is_mouse_pressed()
     board.draw_board()
+    board.check_grid(mouseX,mouseY)
+    
 
 def is_mouse_pressed():
     global radius
@@ -63,8 +101,6 @@ def is_mouse_pressed():
         radius = 12.5
     else:
         radius = 25
-
-
 
 #def can_place():
 
