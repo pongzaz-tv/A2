1a33e65bfd2c078736bcdb2552475bb2d5bb2254
Chayapon Yaram <pongisnotgood3@gmail.com>
2026-09-22 15:21:47 +0700

    add block shape matrix

--- Chayapon/a2.py
+++ Chayapon/a2.py
@@ -4,6 +4,43 @@ import random
 grid_size = 50
 center_board_x = 250
 center_board_y = 200
+board_info =   [[0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0]]
+
+shape_dot = [[1]]
+
+shape_2_downl = [[1,1]]
+shape_2_upl =   [[1],
+                 [1]]
+
+shape_3_downl = [[1,1,1]]
+shape_3_upl = [[1],[1,[1]]]
+
+shape_4_full =  [[1,1],
+                 [1,1]]
+shape_4_downl = [[1,1,1,1]]
+shape_4_upl =  [[1],
+                [1],
+                [1],
+                [1]]
+shape_4_left_topr = [[1,1],
+                    [1,0]] 
+shape_4_left_topl = [[1,1],
+                     [0,1]]
+shape_4_left_downr = [[1,0],
+                      [1,1]]  
+shape_4_left_downl = [[0,1],
+                      [1,1]] 
+
+shape_9_full = [[1,1,1],
+                [1,1,1],
+                [1,1,1]]
 
 PALETTE =  [(245, 93, 62),   # Orange-Red
             (66, 133, 244),  # Blue
@@ -11,17 +48,17 @@ PALETTE =  [(245, 93, 62),   # Orange-Red
             (251, 188, 5),   # Yellow
             (171, 71, 188)]  # Purple
 
-#SHAPE_TEMPLATES =  
-
+SHAPE_TEMPLATES = [shape_dot,shape_2_downl,shape_2_upl,shape_3_downl,shape_3_upl,shape_4_full,shape_4_downl,shape_4_upl,shape_4_left_topr,shape_4_left_topl,shape_4_left_downr,shape_4_left_downl,shape_9_full]
 
 class Board:
     cbx = cby = gs = gr = 0
-
-    def __init__(self,center_board_x,center_board_y,grid_size):
+    board_info = []
+    def __init__(self,center_board_x,center_board_y,grid_size,board_info):
         self.cbx = center_board_x -200
         self.cby = center_board_y -175
         self.gs = grid_size
         self.gr = 50
+        self.board_info = board_info
 
     def draw_board(self): #loop draw ทำให้
         draw_cbx = self.cbx
@@ -36,6 +73,7 @@ class Board:
             line(draw_cbx,draw_cby,draw_cbx+400,draw_cby)
             draw_cby += 50
             iy += 1
+
     def check_grid(self,x,y):
         matrix_x = 0
         matrix_y = 0
@@ -75,17 +113,27 @@ class Board:
         text(matrix_y,mouseX,mouseY)
         text(matrix_x,mouseX+4,mouseY)
 
+    def can_place(self):
+
+
+
+    def place():
+
 
 radius = 25
-"""
+
 class Piece:
-    def __init__():
-"""
+    bms = []
+    colour = 0
+    def __init__(self,block_matrix_scheme,colour):
+        self.bms = block_matrix_scheme
+        self.colour = colour
+
 def setup():
     global board
     size(500,600)
     frameRate(60)
-    board = Board(center_board_x,center_board_y,grid_size)
+    board = Board(center_board_x,center_board_y,grid_size,board_info)
 
 def draw():
     background(225)
@@ -94,7 +142,6 @@ def draw():
     board.draw_board()
     board.check_grid(mouseX,mouseY)
     
-
 def is_mouse_pressed():
     global radius
     if mousePressed:
@@ -102,13 +149,7 @@ def is_mouse_pressed():
     else:
         radius = 25
 
-#def can_place():
-
-#def place():
-
 #def clear_lines():
-
-#def mousePressed():
 
 #def mouseDragged():
 
